5b24bcb6727e15934c5a14900e552c1f2d420417
Gustavo Eric Alves Melo <gugaben28@gmail.com>
2024-08-27 22:16:22 -0300

    CRUD-Products

--- API/server.js
+++ API/server.js
@@ -1,15 +1,43 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 import express from 'express'
 
 const app = express()
 app.use(express.json())
 
-app.post('/produtos',(req,res)=>{
+app.post('/produtos',async (req,res)=>{
+    await prisma.product.create({
+        data:{
+            name: req.body.name,
+            marca: req.body.marca
+        }
+    })
     res.status(201).json(req.body)
 })
-app.get('/produtos',(req,res)=>{
-    res.send('OK')
+app.get('/produtos',async(req,res)=>{
+    const products =  await prisma.product.findMany()
+    res.status(200).json(products)
 })
-app.put('')
-app.delete('')
+app.put('/produtos/:id',async (req,res)=>{
+    await prisma.product.update({
+        where:{
+            id: req.params.id
+        },
+        data:{
+            name: req.body.name,
+            marca: req.body.marca
+        }
+    })
+    res.status(200).json(req.body)
+})
+app.delete('/produtos/:id',async (req,res)=>{
+    await prisma.product.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message:"Usuario Deletado"})
+})
 
 app.listen(3000)
